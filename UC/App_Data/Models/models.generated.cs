b4297e04c5a899b9132492279ac8bcbbcc76c766
tuanapp <kdx.shaggy@gmail.com>
2017-06-28 17:44:50 +0530

    Adding basic folders to umbraco solution
    Adding basic folders to umbraco solution

--- UC/App_Data/Models/models.generated.cs
+++ UC/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "b571d7b9eef08fdc")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
